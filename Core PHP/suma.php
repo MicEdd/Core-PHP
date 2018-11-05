5dd742772633656fc38e9d78bf80b8165ea1dcc1
MicEdd <44681445+MicEdd@users.noreply.github.com>
2018-11-05 21:00:00 +0200

    Add files via upload

---
+++ Core PHP/suma.php
@@ -0,0 +1,8 @@
+<?php
+  echo 'Introduceti primul numar: ';
+  $nr1 = readline();
+  echo 'Introduceti al doilea numar: ';
+  $nr2 = readline();
+  $sum = $nr1 + $nr2;
+  echo "Suma este: $sum".PHP_EOL;
+ ?>
